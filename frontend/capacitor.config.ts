1f066323d42ce2863ee143f721ac2a27fad40d09
Dewatredolink <acatdewa@gmail.com>
2026-09-12 20:44:24 +0530

    Remove deprecated Capacitor bundledWebRuntime option

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.dewatredolink.pmposhan',
   appName: 'PM POSHAN',
   webDir: 'dist',
-  bundledWebRuntime: false,
   android: {
     allowMixedContent: false,
   },
